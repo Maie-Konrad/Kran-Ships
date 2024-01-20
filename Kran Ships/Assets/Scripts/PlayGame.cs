aff18b481ab24cd78592dcd49288705a6cd405ea
Maie-Konrad <anononanonowski25@gmail.com>
2024-01-20 20:49:37 +0100

    20-1-24

--- Kran Ships/Assets/Scripts/PlayGame.cs
+++ Kran Ships/Assets/Scripts/PlayGame.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour
 {
+    public Animator transitionCrossF;
+    [SerializeField] float transtionWait;
     public void Playgame()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(Playgame(SceneManager.GetActiveScene().buildIndex + 1));
 
     }
+
+    IEnumerator Playgame(int levelIndex)
+    {
+        
+        transitionCrossF.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transtionWait);
+
+        SceneManager.LoadScene(levelIndex);
+    }   
     public void QuitGame()
     {
         Application.Quit();
